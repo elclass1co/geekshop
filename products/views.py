86d5817269112cf6e27433e1e0bfbbee9df71b63
Nikita Shataev <shataev.nikita5@gmail.com>
2021-05-31 23:18:54 +0700

    lesson2_homework not finished

--- products/views.py
+++ products/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'GeekShop - каталог',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.', 'description':
+                'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.', 'description':
+                'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.', 'description  ':
+                'Материал с плюшевой текстурой. Удобный и мягкий.'},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': '13 590,00 руб.', 'description  ':
+                'Плотная ткань. Легкий материал.'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '2 340,00 руб.',
+             'description  ':
+                 'Гладкий кожаный верх. Натуральный материал.'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2 890,00 руб.', 'description  ':
+                'Легкая эластичная ткань сирсакер Фактурная ткань.'},
+        ]
+    }
+
+    return render(request, 'products/products.html', context)
