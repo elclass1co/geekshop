6c8f9a672b3a52f33b34f5523bdee8690ee0464a
Nikita Shataev <shataev.nikita5@gmail.com>
2021-06-09 15:40:29 +0700

    created UserLoginForm

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
 # Create your views here.
 
 def login(request):
+    form = UserLoginForm()
     context = {
-        'title': 'GeekShop - Авторизация'
+        'title': 'GeekShop - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
