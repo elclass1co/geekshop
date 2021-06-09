b40cc018ca1867c4ca71270f627c6f7ceada42a9
Nikita Shataev <shataev.nikita5@gmail.com>
2021-06-09 17:35:15 +0700

    final steps

--- users/views.py
+++ users/views.py
@@ -42,3 +42,7 @@ def register(request):
         'form': form
     }
     return render(request,'users/register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
