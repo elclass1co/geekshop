a56d37e87dcc8bc5bd35d921e920859e96571774
Nikita Shataev <shataev.nikita5@gmail.com>
2021-06-14 16:15:45 +0700

    added profile template

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import login, register, logout
+from users.views import login, register, logout, profile
 
 
 app_name = 'users'
@@ -7,5 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout')
 ]
